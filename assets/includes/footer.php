ba9014f5c8173ef643531c17b3ca274c743bcb55
glhwbsn <galehwibisono270101@gmail.com>
2026-04-24 05:25:37 +0700

    Perubahan href css dan hapus title

--- assets/includes/footer.php
+++ assets/includes/footer.php
@@ -3,10 +3,9 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Footer RentalKu</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-  <link rel="stylesheet" href="../css/footer.css">
+  <link rel="stylesheet" href="assets/css/footer.css">
 </head>
 <body>
   <footer class="footer-rental">
