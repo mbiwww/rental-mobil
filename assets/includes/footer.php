a9a4cf803bf1c72ada84541462856214223d20cc
glhwbsn <galehwibisono270101@gmail.com>
2026-04-25 19:14:10 +0700

    Hapus link CSS

--- assets/includes/footer.php
+++ assets/includes/footer.php
@@ -5,7 +5,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-  <link rel="stylesheet" href="assets/css/footer.css">
 </head>
 <body>
   <footer class="footer-rental">
