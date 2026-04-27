538d2bbc9f2e3ce7ff20768eaa506b667a6ba21a
glhwbsn <galehwibisono270101@gmail.com>
2026-04-27 10:02:27 +0700

    Merubah header menjadi navbar, dan menambahkan footer

--- pages/keranjang.php
+++ pages/keranjang.php
@@ -5,43 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Keranjang - RentalKu</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="stylesheet" href="../assets/css/navbar.css">
+    <link rel="stylesheet" href="../assets/css/footer.css">
     <style>
         body {
             font-family: Arial, sans-serif;
             margin: 0;
             padding: 0;
             background-color: #fff;
-        }
-
-        /* Header Style */
-        header {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            padding: 15px 50px;
-            border-bottom: 1px solid #eee;
-        }
-
-        .logo {
-            font-weight: bold;
-            font-size: 24px;
-            color: #000;
-            display: flex;
-            align-items: center;
-            gap: 10px;
-        }
-
-        nav a {
-            text-decoration: none;
-            color: #333;
-            margin-right: 25px;
-            font-weight: 500;
-        }
-
-        .header-icons {
-            display: flex;
-            gap: 20px;
-            font-size: 18px;
         }
 
         /* Empty Cart Section */
@@ -88,20 +59,9 @@
 </head>
 <body>
 
-    <header>
-        <div class="logo">
-            <i class="fas fa-car-side" style="color: #2b67f6;"></i> RentalKu
-        </div>
-        <div style="display: flex; align-items: center;">
-            <nav>
-                <a href="katalog.php">Katalog Mobil</a>
-            </nav>
-            <div class="header-icons">
-                <a href="keranjang.php" style="color: #000;"><i class="fas fa-shopping-cart"></i></a>
-                <a href="profile.php" style="color: #000;"><i class="far fa-user"></i></a>
-            </div>
-        </div>
-    </header>
+  <?php
+  include '../assets/includes/navbar.php';
+  ?>
 
     <main class="empty-cart-container">
         <i class="fas fa-shopping-cart"></i>
@@ -111,6 +71,6 @@
         
         <a href="katalog.php" class="btn-rent">Mulai Menyewa Mobil</a>
     </main>
-
+<?php include '../assets/includes/footer.php'; ?>
 </body>
 </html>
