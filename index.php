6a60141706a9dd610e52a7f01dd7f5c7545848cd
glhwbsn <galehwibisono270101@gmail.com>
2026-05-25 12:51:24 +0700

    ubah direktori navbar dan footer

--- index.php
+++ index.php
@@ -12,9 +12,8 @@
   <link rel="stylesheet" href="assets/css/footer.css">
 </head>
 <body>
-  <?php
-  include 'assets/includes/navbar.php';
-  ?>
+  <?php include 'includes/navbar.php'; ?>
+  
 <section class="hero-section">
   <div class="container hero-content">
     <div class="row align-items-center g-4">
@@ -172,6 +171,6 @@
 <!-- Bootstrap JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
-<?php include 'assets/includes/footer.php'; ?>
+<?php include 'includes/footer.php'; ?>
 </body>
 </html>
